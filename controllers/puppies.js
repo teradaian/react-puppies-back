a7e859e70fedfffb80e937221231ee04384dcdbb
teradaian <teradaian@gmail.com>
2023-01-25 08:28:30 -0800

    show and create

--- controllers/puppies.js
+++ controllers/puppies.js
@@ -1,28 +1,53 @@
-import { Puppy } from '../models/puppy'
+import { Puppy } from '../models/puppy.js'
 
 const index = async(req, res) => {
   try{
-
+    const puppies = await Puppy.find({})
+    res.send(200).json(puppies)
   } catch(err){
-
+    console.log(err)
+    res.send(500).json(err)
   }
 }
 
 
 const show = async(req, res) => {
-
+  try{
+    let puppy = Puppy.findById(req.params.id)
+    res.send(200).json(puppy)
+  } catch(err){
+    console.log(err)
+    res.send(500).json(err)
+  }
 }
 
 const createPuppy= async(req, res) => {
+  try {
+    let puppy = await Puppy.create(req.body)
+    res.send(200).json(puppy)
+  } catch(err){
+    console.log(err)
+    res.send(500).json(err)
+  }
 
 }
 
 const update = async(req, res) => {
+  try{
 
+  } catch(err){
+    console.log(err)
+    res.send(500).json(err)
+  }
 }
 
 const deletePuppy = async(req, res) => {
+  try{
 
+  } catch(err){
+    console.log(err)
+    res.send(500).json(err)
+  }
 }
 
 
