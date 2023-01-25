bce9fcdf93635229a14ec1bc4bcb1e57e2e61538
teradaian <teradaian@gmail.com>
2023-01-25 08:16:02 -0800

    rename user routing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import('./config/database.js')
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
-import { router as usersRouter } from './routes/users.js'
+import { router as puppiesRouter } from './routes/puppies.js'
 
 // create the express app
 const app = express()
@@ -30,7 +30,7 @@ app.use(methodOverride('_method'))
 
 // mount imported routes
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/puppies', puppiesRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
