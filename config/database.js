f6b8189c066055b4022e36ab93ab766fd137d266
teradaian <teradaian@gmail.com>
2023-01-25 08:09:00 -0800

    server config

---
+++ config/database.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+
+const db = mongoose.connection
+db.on('connected', () => {
+    console.log(`Connected to ${db.name}`)
+})
